75d74089219a404f9e7845c7657f26e1ebf4ca0b
Chris <chrismtz2003@gmail.com>
2026-08-10 01:16:39 -0400

    Made API call to check fo response

--- Lab17_cmartinez42-1.py
+++ Lab17_cmartinez42-1.py
@@ -6,3 +6,12 @@ Starter Code:'hn_submissions.py' from Chapter 17
 Date: 8/10/2026
 """
 
+import requests
+
+url = "https://hacker-news.firebaseio.com/v0/topstories.json"
+r = requests.get(url)
+print(f"Status code: {r.status_code}")
+
+
+
+
