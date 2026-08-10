f4b366ee7b7725206c84b27d95d2a6c27ca50d96
Chris <chrismtz2003@gmail.com>
2026-08-10 01:12:47 -0400

    Added my docstring

---
+++ Lab17_cmartinez42-1.py
@@ -0,0 +1,8 @@
+"""
+Program Name: Hacker News Article Fetcher
+Author: Chris Martinez
+Purpose: Gets popular articles from Hacker News and prints them without crashing if comments are missing.
+Starter Code:'hn_submissions.py' from Chapter 17
+Date: 8/10/2026
+"""
+
