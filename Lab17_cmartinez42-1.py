acc8fc7896c6ff63a470a0f24904066d9d61538d
Chris <chrismtz2003@gmail.com>
2026-08-10 01:35:19 -0400

    Looped through top 30 stories and printed details

--- Lab17_cmartinez42-1.py
+++ Lab17_cmartinez42-1.py
@@ -14,4 +14,16 @@ print(f"Status code: {r.status_code}")
 
 submission_ids = r.json()
 
+for submission_id in submission_ids[:30]:
+    url = f"https://hacker-news.firebaseio.com/v0/item/{submission_id}.json"
+    r = requests.get(url)
+    response_dict = r.json()
+
+    title = response_dict.get("title", "No Title")
+    comments = response_dict.get("descendants", 0)
+
+    print(f"\nTitle: {title}")
+    print(f"Discussion Link: http://news.ycombinator.com/item?id={submission_id}")
+    print(f"Comments: {comments}")
+
 
